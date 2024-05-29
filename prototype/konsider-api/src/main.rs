0f448dfd82f51fca24835408ae057a39c1b63620
James <jamesonley@outlook.com>
2024-05-28 20:01:24 -0400

    Added initial API code

--- prototype/konsider-api/src/main.rs
+++ prototype/konsider-api/src/main.rs
@@ -1,3 +1,57 @@
-fn main() {
-    println!("Hello, world!");
+
+// Author1: James Onley
+// Date: May 28, 2024
+// Description: This code uses the Rust Rocket framework to implement a basic API with several endpoints. 
+
+#[macro_use] extern crate rocket;
+use rocket::tokio::time::{ sleep, Duration} ;
+use std::sync::atomic::AtomicUsize;
+use rocket::State;
+use std::sync::atomic::Ordering;
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Index: Hello, world!"
+}
+
+#[get("/world")]
+fn world() -> &'static str {
+    "World: Hello, world!"
+}
+
+#[get("/delay/<seconds>")]
+async fn delay(seconds: u64) -> String {
+    sleep(Duration::from_secs( seconds )).await;
+    format!("Delay: Waited for {} seconds", seconds)
+}
+
+#[get("/hello/<name>/<age>/<cool>")]
+fn hello(name: &str, age: u8, cool: bool) -> String {
+    if cool {
+        format!("Hello: You're a cool {} year old, {}!", age, name)
+    } else {
+        format!("Hello: {}, we need to talk about your coolness.", name)
+    }
+}
+
+struct HitCount{
+    count: AtomicUsize
+}
+
+#[get("/count")]
+fn count(hit_count:&State<HitCount>) -> String {
+    let current_count = hit_count.count.load(Ordering::Relaxed);
+    format!("Count: Number of visits: {}", current_count)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .manage(HitCount { count: AtomicUsize::new(0) })
+        .mount("/", routes![index])
+        .mount("/", routes![world])
+        .mount("/", routes![delay])
+        .mount("/", routes![hello])
+        .mount("/", routes![count])      
 }
