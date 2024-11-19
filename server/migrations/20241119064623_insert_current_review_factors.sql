579d2e0b44d41a13aa482e4925a8113b45e92bc4
Huseen <136820327+hmunye@users.noreply.github.com>
2024-11-19 01:48:10 -0500

    chore: created migration to insert current review factors

---
+++ server/migrations/20241119064623_insert_current_review_factors.sql
@@ -0,0 +1,10 @@
+INSERT INTO review_factor (name, description) VALUES
+    ('is_supported', 'Is the software still supported by the developer?'),
+    ('is_current_version', 'Is the latest version of the software being requested?'),
+    ('is_reputation_good', 'Does the developer have a good reputation?'),
+    ('is_installation_from_developer', 'Is the installation package from the developer/vendor?'),
+    ('is_local_admin_required', 'Is a local administrator required for daily use?'),
+    ('is_connected_to_brockport_cloud', 'Does the software need to connect to Brockport cloud?'),
+    ('is_connected_to_cloud_services_or_client', 'Does the software need to connect to other cloud services or is a client for a cloud service?'),
+    ('is_security_or_optimization_software', 'Is the software for security or system optimization?'),
+    ('is_supported_by_current_os', 'Is the software supported by the current OS used by devices on campus?');
