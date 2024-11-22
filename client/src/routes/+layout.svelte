6b580b99c39789746aad7287ea669417d0184b67
Huseen <136820327+hmunye@users.noreply.github.com>
2024-11-22 00:09:01 -0500

    feat: initial commit of svelte client. created ci workflow for client

---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import "../app.css";
+
+let { children } = $props();
+</script>
+
+<main class="min-h-screen flex flex-col items-center antialiased">
+    <section class="flex flex-1 flex-col w-full items-center">
+        {@render children()}
+    </section>
+</main>
---
+++ client/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-1 flex-row w-full items-center px-3 animate-in">
+    <div class="hidden w-[400px] ml-32 lg:block">
+        <h1 class="text-7xl font-bold">Konsider</h1>
+    </div>
+</div>
