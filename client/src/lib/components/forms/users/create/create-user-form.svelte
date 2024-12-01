0e97aa5ee3501b465c228f3088a968d59edf2e74
Huseen <136820327+hmunye@users.noreply.github.com>
2024-12-01 14:22:16 -0500

    fix: updated styling of forms for consistency

--- client/src/lib/components/forms/users/create/create-user-form.svelte
+++ client/src/lib/components/forms/users/create/create-user-form.svelte
@@ -1,136 +1,140 @@
 <script lang="ts">
-import { PUBLIC_BASE_API_URL } from "$env/static/public";
-import * as Form from "$lib/components/ui/form";
-import { Input } from "$lib/components/ui/input";
-import * as Select from "$lib/components/ui/select";
-import { fetchRequest } from "$lib/fetch";
-import { toast } from "svelte-sonner";
-import { defaults, superForm } from "sveltekit-superforms";
-import { zod, zodClient } from "sveltekit-superforms/adapters";
-import { createUserSchema } from "./schema";
+    import { PUBLIC_BASE_API_URL } from "$env/static/public";
+    import * as Form from "$lib/components/ui/form";
+    import { Input } from "$lib/components/ui/input";
+    import * as Select from "$lib/components/ui/select";
+    import { fetchRequest } from "$lib/fetch";
+    import { toast } from "svelte-sonner";
+    import { defaults, superForm } from "sveltekit-superforms";
+    import { zod, zodClient } from "sveltekit-superforms/adapters";
+    import { createUserSchema } from "./schema";
 
-let submitting: boolean = $state(false);
+    let submitting: boolean = $state(false);
 
-const initialData = {
-  name: "",
-  email: "",
-  password: "",
-  role: "",
-};
+    const initialData = {
+        name: "",
+        email: "",
+        password: "",
+        role: "",
+    };
 
-const form = superForm(defaults(initialData, zod(createUserSchema)), {
-  validators: zodClient(createUserSchema),
-  SPA: true,
-  dataType: "json",
-  resetForm: false,
-  async onUpdate({ form }) {
-    submitting = true;
+    const form = superForm(defaults(initialData, zod(createUserSchema)), {
+        validators: zodClient(createUserSchema),
+        SPA: true,
+        dataType: "json",
+        resetForm: false,
+        async onUpdate({ form }) {
+            submitting = true;
 
-    if (!form.valid) {
-      submitting = false;
-      return;
-    }
-
-    const createUserResponse = new Promise<unknown>((resolve, reject) => {
-      // Simulate a timeout before making the request to show loading toast
-      setTimeout(() => {
-        fetchRequest<unknown>({
-          url: `${PUBLIC_BASE_API_URL}/api/v1/users`,
-          method: "POST",
-          requestBody: {
-            name: $formData.name,
-            email: $formData.email,
-            password: $formData.password,
-            role: $formData.role,
-          },
-        })
-          .then((response) => {
-            if (response.error) {
-              reject(response.error.message);
-            } else {
-              resolve(response);
+            if (!form.valid) {
+                submitting = false;
+                return;
             }
-          })
-          .catch((error) => {
-            reject(error);
-          });
-      }, 2000);
+
+            const createUserResponse = new Promise<unknown>(
+                (resolve, reject) => {
+                    // Simulate a timeout before making the request to show loading toast
+                    setTimeout(() => {
+                        fetchRequest<unknown>({
+                            url: `${PUBLIC_BASE_API_URL}/api/v1/users`,
+                            method: "POST",
+                            requestBody: {
+                                name: $formData.name,
+                                email: $formData.email,
+                                password: $formData.password,
+                                role: $formData.role,
+                            },
+                        })
+                            .then((response) => {
+                                if (response.error) {
+                                    reject(response.error.message);
+                                } else {
+                                    resolve(response);
+                                }
+                            })
+                            .catch((error) => {
+                                reject(error);
+                            });
+                    }, 2000);
+                },
+            );
+
+            toast.promise(createUserResponse, {
+                loading: "Loading...",
+                success: () => {
+                    submitting = false;
+                    return `${$formData.name} has been successfully registered`;
+                },
+                error: (error) => {
+                    submitting = false;
+                    return `${error}`;
+                },
+            });
+        },
     });
 
-    toast.promise(createUserResponse, {
-      loading: "Loading...",
-      success: () => {
-        submitting = false;
-        return `${$formData.name} has been successfully registered`;
-      },
-      error: (error) => {
-        submitting = false;
-        return `${error}`;
-      },
-    });
-  },
-});
+    const { form: formData, enhance } = form;
 
-const { form: formData, enhance } = form;
-
-let selectedRole = $derived(
-  $formData.role
-    ? {
-        label: $formData.role,
-        value: $formData.role,
-      }
-    : undefined,
-);
+    let selectedRole = $derived(
+        $formData.role
+            ? {
+                  label:
+                      $formData.role.charAt(0) +
+                      $formData.role.slice(1).toLowerCase(),
+                  value: $formData.role,
+              }
+            : undefined,
+    );
 </script>
 
 <form method="POST" use:enhance>
     <div class="flex flex-col gap-8 [&>input]:mb-4 mt-8 rounded-lg p-8 py-8">
-        <h1 class="text-4xl font-bold mb-4">Create New User</h1>
+        <h1 class="text-2xl font-bold mb-4">Create New User</h1>
 
         <Form.Field {form} name="name">
             <Form.Control let:attrs>
-                <Form.Label class="text-2xl">Name</Form.Label>
+                <Form.Label class="text-xl">Name</Form.Label>
                 <Input
                     {...attrs}
                     bind:value={$formData.name}
                     type="text"
                     autocomplete="name"
                     placeholder="John"
-                    class="text-xl placeholder:text-xl placeholder:font-light"
+                    class="text-lg placeholder:text-lg placeholder:font-light"
                 />
             </Form.Control>
             <Form.FieldErrors class="text-lg" />
         </Form.Field>
         <Form.Field {form} name="email">
             <Form.Control let:attrs>
-                <Form.Label class="text-2xl">Email</Form.Label>
+                <Form.Label class="text-xl">Email</Form.Label>
                 <Input
                     {...attrs}
                     bind:value={$formData.email}
                     type="email"
                     autocomplete="email"
                     placeholder="you@example.com"
-                    class="text-xl placeholder:text-xl placeholder:font-light"
+                    class="text-lg placeholder:text-lg placeholder:font-light"
                 />
             </Form.Control>
             <Form.FieldErrors class="text-lg" />
         </Form.Field>
         <Form.Field {form} name="password">
             <Form.Control let:attrs>
-                <Form.Label class="text-2xl">Password</Form.Label>
+                <Form.Label class="text-xl">Password</Form.Label>
                 <Input
                     {...attrs}
                     bind:value={$formData.password}
                     type="password"
                     placeholder="••••••••"
-                    class="text-xl placeholder:text-xl placeholder:font-light"
+                    class="text-lg placeholder:text-lg placeholder:font-light"
                 />
             </Form.Control>
             <Form.FieldErrors class="text-lg" />
         </Form.Field>
         <Form.Field {form} name="role">
             <Form.Control let:attrs>
-                <Form.Label class="text-2xl">Role</Form.Label>
+                <Form.Label class="text-xl">Role</Form.Label>
                 <Select.Root
                     selected={selectedRole}
                     onSelectedChange={(v) => {
@@ -140,18 +144,18 @@ let selectedRole = $derived(
                     <Select.Trigger {...attrs}>
                         <Select.Value
                             placeholder="Select User Role"
-                            class="text-xl placeholder:text-xl placeholder:font-light"
+                            class="text-lg placeholder:text-lg placeholder:font-light"
                         />
                     </Select.Trigger>
                     <Select.Content>
                         <Select.Item
                             value="ADMIN"
-                            label="ADMIN"
+                            label="Admin"
                             class="text-md"
                         />
                         <Select.Item
                             value="REVIEWER"
-                            label="REVIEWER"
+                            label="Reviewer"
                             class="text-md"
                         />
                     </Select.Content>
@@ -161,7 +165,7 @@ let selectedRole = $derived(
             <Form.FieldErrors />
         </Form.Field>
         <Form.Button
-            class="bg-success text-success-foreground text-xl hover:bg-success hover:brightness-125 transition duration-300"
+            class="bg-success text-success-foreground text-lg hover:bg-success hover:brightness-125 transition duration-300"
             disabled={submitting}
             aria-disabled={submitting}
         >
